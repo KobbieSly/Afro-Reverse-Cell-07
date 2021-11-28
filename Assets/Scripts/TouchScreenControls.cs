72baf61014ecf87df4feeac0a97eb51d3c5df44d
KobbieSly <91862236+KobbieSly@users.noreply.github.com>
2021-11-28 07:57:54 +0000

    Add files via upload

--- Assets/Scripts/TouchScreenControls.cs
+++ Assets/Scripts/TouchScreenControls.cs
@@ -1,43 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchScreenControls : MonoBehaviour
-{
-    
-    public GameObject character;
+{    
+    private Vector2 startTouchPosition;
+    private Vector2 currentPosition;
+    private Vector2 endTouchPosition;
+    private bool stopTouch = false;
 
-    
-    private float ScreenWidth;
-
-
-    // Use this for initialization
-    void Start()
-    {
-        ScreenWidth = Screen.width;
-    }
+    public float swipeRange =40;
+    public float tapRange =10;
 
     // Update is called once per frame
     void Update()
     {
-        int i = 0;
-        //loop over every touch found
-        while (i < Input.touchCount)
-        {
-            if (Input.GetTouch(i).position.x > ScreenWidth / 2)
-            {
-                //jump up
-                gameObject.GetComponent<PlayerController2>().JumpFunction();
-            }
-            if (Input.GetTouch(i).position.x < ScreenWidth / 2)
-            {
-                //slide on the ground
-                gameObject.GetComponent<PlayerController2>().SlideFunction();
-            }
-            ++i;
-        }
+        Swipe();
     }
- 
 
-   
+    public void Swipe()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            currentPosition = Input.GetTouch(0).position;
+            Vector2 Distance = currentPosition - startTouchPosition;
+
+            if (!stopTouch)
+            {
+
+                
+                if (Distance.y > swipeRange)
+                {
+                    gameObject.GetComponent<PlayerController2>().JumpFunction();
+                    stopTouch = true;
+                }
+                else if (Distance.y < -swipeRange)
+                {
+                    gameObject.GetComponent<PlayerController2>().SlideFunction();
+                    stopTouch = true;
+                }
+
+            }
+
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            stopTouch = false;
+
+            endTouchPosition = Input.GetTouch(0).position;
+
+            Vector2 Distance = endTouchPosition - startTouchPosition;
+
+           /* if (Mathf.Abs(Distance.x) < tapRange && Mathf.Abs(Distance.y) < tapRange)
+            {
+                outputText.text = "Tap";
+            }*/
+
+        }
+
+
+    }
 }
